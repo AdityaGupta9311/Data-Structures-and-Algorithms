5c1e4818c59ab670c7a027c1547022951ca33bf1
Aditya Gupta <aditya.gupta93117@gmail.com>
2025-01-19 18:18:48 +0530

    Bubble Sort

--- Sorting/BubbleSort.java
+++ Sorting/BubbleSort.java
@@ -6,7 +6,7 @@ public class BubbleSort {
     static void sort(int[] arr) {
         boolean swapped;
         for (int i = 0; i < arr.length - 1; i++) {
-            swapped=false;
+            swapped = false;
             for (int j = 1; j < arr.length - i; j++) {
                 if (arr[j] < arr[j - 1]) {
                     int temp = arr[j];
@@ -15,12 +15,13 @@ public class BubbleSort {
                     swapped = true;
                 }
             }
-            if (!swapped) break;
+            if (!swapped)
+                break;
         }
     }
 
     public static void main(String[] args) {
-        int[] arr = { 10,9,8,7,6,5,4,3,2,1,0 };
+        int[] arr = { 10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0 };
         sort(arr);
         System.out.println(Arrays.toString(arr));
     }
