e1cefb116b086a7a5c22ad9df21ee476a397e8be
Aditya Gupta <aditya.gupta93117@gmail.com>
2025-01-20 00:38:01 +0530

    Insertion Sort

---
+++ Sorting/InsertionSort.java
@@ -0,0 +1,23 @@
+package Sorting;
+
+import java.util.Arrays;
+
+public class InsertionSort {
+    static void sort(int[] arr) {
+        for (int i = 0; i < arr.length - 1; i++) {
+            for (int j = i + 1; j > 0; j--) {
+                if (arr[j] < arr[j - 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j - 1];
+                    arr[j - 1] = temp;
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] arr = { 3, 5, 2, 42, 4, 556, 75, -2, 30, 0 };
+        sort(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+}
