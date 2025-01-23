00c98513450a1b9b0babd0435805bb71c914bc7b
Aditya Gupta <aditya.gupta93117@gmail.com>
2025-01-23 16:36:57 +0530

    Pattern

--- Pattern/Pattern.java
+++ Pattern/Pattern.java
@@ -2,7 +2,7 @@ package Pattern;
 
 public class Pattern {
 
-    static void pattern(){
+    static void pattern() {
         for (int i = 1; i <= 5; i++) {
             for (int j = 0; j < i; j++) {
                 System.out.print("*");
@@ -12,7 +12,7 @@ public class Pattern {
         System.out.println();
     }
 
-    static void pattern1(){
+    static void pattern1() {
         for (int i = 1; i <= 5; i++) {
             for (int j = 0; j < 5; j++) {
                 System.out.print("*");
@@ -21,7 +21,7 @@ public class Pattern {
         }
     }
 
-    static void pattern2(){
+    static void pattern2() {
         for (int i = 0; i <= 5; i++) {
             for (int j = 0; j <= 5 - i; j++) {
                 System.out.print("*");
@@ -30,7 +30,26 @@ public class Pattern {
         }
     }
 
+    static void pattern3() {
+        for (int i = 1; i <= 5; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(j);
+            }
+            System.out.println();
+        }
+    }
+
+    static void pattern4() {
+        for (int i = 0; i <= 5*2; i++) {
+            int colinrow = i > 5 ? 2 * 5 - i : i;
+            for (int j = 0; j < colinrow; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+
     public static void main(String[] args) {
-        pattern2();
+        pattern4();
     }
 }
