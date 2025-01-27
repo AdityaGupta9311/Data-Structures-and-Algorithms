462544fa6f202539aa0f78e2d453d7bd3d28cce4
Aditya Gupta <aditya.gupta93117@gmail.com>
2025-01-27 18:10:43 +0530

    BitWise

--- BitWise/Pratice1.java
+++ BitWise/Pratice1.java
@@ -4,8 +4,19 @@ public class Pratice1 {
         return (n & 1) == 1;
     }
 
+    public static int singlevalue(int[] arr) {
+        int unique = 0;
+        for (int i : arr) {
+            unique ^= i;
+        }
+        return unique;
+    }
+
     public static void main(String[] args) {
-        int n = 21;
-        System.out.println(isOdd(n));
+        // int n = 21;
+        // System.out.println(isOdd(n));
+
+        int[] arr = { 2, 3, 4, 5, 3, 2, 4 };
+        System.out.println(singlevalue(arr));
     }
 }
