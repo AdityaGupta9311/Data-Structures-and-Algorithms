c08970ccec0252e2c49b4ca556364a3408934e63
Aditya Gupta <aditya.gupta93117@gmail.com>
2025-02-08 18:01:52 +0530

    Fibonacci

--- Recursion/Fibonacci.java
+++ Recursion/Fibonacci.java
@@ -2,11 +2,11 @@ package Recursion;
 
 public class Fibonacci {
 
-    static int fibo(int n){
-        if(n<2){
+    static int fibo(int n) {
+        if (n < 2) {
             return n;
         }
-        return fibo(n-1)+fibo(n-2);
+        return fibo(n - 1) + fibo(n - 2);
     }
 
     public static void main(String[] args) {
